66647b6c783d60d35c63fa6b239b9b3160d03c41
he <slut@vip.qq.com>
2021-04-09 17:19:26 +0800

    修改spring-cloud-gateway的redis配置

--- spring-cloud-gateway/src/main/java/com/example/config/RedisConfig.java
+++ spring-cloud-gateway/src/main/java/com/example/config/RedisConfig.java
@@ -2,12 +2,6 @@ package com.example.config;
 
 import java.util.Arrays;
 
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
-import org.redisson.api.RedissonReactiveClient;
-import org.redisson.config.ClusterServersConfig;
-import org.redisson.config.Config;
-import org.redisson.spring.data.connection.RedissonConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +9,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.*;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -27,34 +22,34 @@ public class RedisConfig {
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-	@Bean(destroyMethod = "shutdown", name = "redissonClient")
+	@Bean(name = "redisConfiguration")
 	@ConditionalOnProperty(name = "spring.redis.type", havingValue = "0")
-	public RedissonClient redissonClient(@Value("${spring.redis.schema}") String schema,
-			@Value("${spring.redis.host}") String host, @Value("${spring.redis.port}") int port,
-			@Value("${spring.redis.database}") int dataBase) {
-		Config config = new Config();
-		String address = schema + "://" + host + ":" + port;
-		config.useSingleServer().setAddress(address).setDatabase(dataBase);
-		log.info("connection redis single server {} database {} success", address, dataBase);
-		return Redisson.create(config);
+	public RedisStandaloneConfiguration redisStandaloneConfiguration(@Value("${spring.redis.host}") String host,
+			@Value("${spring.redis.port}") int port, @Value("${spring.redis.database}") int dataBase) {
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+		redisStandaloneConfiguration.setHostName(host);
+		redisStandaloneConfiguration.setPort(port);
+		redisStandaloneConfiguration.setDatabase(dataBase);
+		log.info("connection redis single server {}:{} database {} success", host, port, dataBase);
+		return redisStandaloneConfiguration;
 	}
 
-	@Bean(destroyMethod = "shutdown", name = "redissonClient")
+	@Bean(name = "redisConfiguration")
 	@ConditionalOnProperty(name = "spring.redis.type", havingValue = "1")
-	public RedissonClient redissonClusterClient(@Value("${spring.redis.schema}") String schema,
-			@Value("${spring.redis.cluster.nodes}") String[] nodes) {
-		Config config = new Config();
-		ClusterServersConfig clusterServersConfig = config.useClusterServers();
+	public RedisClusterConfiguration redisConfiguration(@Value("${spring.redis.cluster.nodes}") String[] nodes) {
+		RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration();
 		for (String node : nodes) {
-			clusterServersConfig.addNodeAddress(schema + "://" + node);
+			String[] address = node.split(":");
+			RedisNode redisNode = new RedisNode(address[0], Integer.parseInt(address[1]));
+			redisClusterConfiguration.addClusterNode(redisNode);
 		}
 		log.info("connection redis cluster server {}", Arrays.toString(nodes));
-		return Redisson.create(config);
+		return redisClusterConfiguration;
 	}
 
 	@Bean
-	public RedisConnectionFactory redissonConnectionFactory(@Autowired RedissonClient redissonClient) {
-		return new RedissonConnectionFactory(redissonClient);
+	public RedisConnectionFactory redisConnectionFactory(@Autowired RedisConfiguration redisConfiguration) {
+		return new LettuceConnectionFactory(redisConfiguration);
 	}
 
 	@Bean
@@ -73,10 +68,5 @@ public class RedisConfig {
 		template.setKeySerializer(new StringRedisSerializer());
 		template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
 		return template;
-	}
-
-	@Bean
-	public RedissonReactiveClient redissonReactive(@Autowired RedissonClient redissonClient) {
-		return redissonClient.reactive();
 	}
 }
--- spring-cloud-gateway/src/main/java/com/example/config/SessionConfig.java
+++ spring-cloud-gateway/src/main/java/com/example/config/SessionConfig.java
@@ -1,31 +1,21 @@
 package com.example.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
-import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
-import org.springframework.session.web.http.HttpSessionIdResolver;
+import org.springframework.web.server.session.HeaderWebSessionIdResolver;
+import org.springframework.web.server.session.WebSessionIdResolver;
 
 @EnableRedisWebSession
+@ConditionalOnProperty(name = "spring.redis.enable", havingValue = "true")
 public class SessionConfig {
 
-	@Bean
-	public RedisStandaloneConfiguration redisStandaloneConfiguration(@Value("${spring.redis.host}") String host,
-			@Value("${spring.redis.port}") int port) {
-		return new RedisStandaloneConfiguration(host, port);
-	}
+	private static final String HEADER_X_AUTH_TOKEN = "X-Auth-Token";
 
 	@Bean
-	public LettuceConnectionFactory lettuceConnectionFactory(
-			@Autowired RedisStandaloneConfiguration redisStandaloneConfiguration) {
-		return new LettuceConnectionFactory(redisStandaloneConfiguration);
-	}
-
-	@Bean
-	public HttpSessionIdResolver httpSessionIdResolver() {
-		return HeaderHttpSessionIdResolver.xAuthToken();
+	public WebSessionIdResolver httpSessionIdResolver() {
+		HeaderWebSessionIdResolver headerWebSessionIdResolver = new HeaderWebSessionIdResolver();
+		headerWebSessionIdResolver.setHeaderName(HEADER_X_AUTH_TOKEN);
+		return headerWebSessionIdResolver;
 	}
 }

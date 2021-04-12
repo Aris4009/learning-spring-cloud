241c2acc2bb419e6df4e266b8f76edb3c6e86c30
he <slut@vip.qq.com>
2021-04-12 18:24:24 +0800

    添加resilience4j

---
+++ spring-cloud-gateway/src/main/java/com/example/config/MyCircuitBreakerConfig.java
@@ -0,0 +1,11 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyCircuitBreakerConfig {
+
+	// public CircuitBreaker myCircuitBreaker() {
+	//
+	// }
+}

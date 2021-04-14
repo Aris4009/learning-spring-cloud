624810567f88ae49e74f6104504155a7b3a2cda1
he <slut@vip.qq.com>
2021-04-14 18:25:38 +0800

    添加全局过滤器

--- service-a/src/main/java/com/example/App.java
+++ service-a/src/main/java/com/example/App.java
@@ -4,8 +4,10 @@ import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {RedissonAutoConfiguration.class, RabbitAutoConfiguration.class})
+@SpringBootApplication(exclude = {RedissonAutoConfiguration.class, RabbitAutoConfiguration.class,
+		DataSourceAutoConfiguration.class})
 public class App {
 
 	public static void main(String[] args) {
---
+++ spring-cloud-gateway/src/main/java/com/example/filter/GlobalFiltersConfig.java
@@ -0,0 +1,28 @@
+package com.example.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.filter.factory.StripPrefixGatewayFilterFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+@Configuration
+public class GlobalFiltersConfig {
+
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+	@Bean
+	@Order(Integer.MIN_VALUE + 1)
+	public GlobalFilter stripPrefixFilter(@Value("${spring.cloud.gateway.global.filters.strip.prefix}") int parts) {
+		StripPrefixGatewayFilterFactory.Config config = new StripPrefixGatewayFilterFactory.Config();
+		config.setParts(parts);
+		return (exchange, chain) -> {
+			log.info("1");
+			StripPrefixGatewayFilterFactory stripPrefixGatewayFilterFactory = new StripPrefixGatewayFilterFactory();
+			return stripPrefixGatewayFilterFactory.apply(config).filter(exchange, chain);
+		};
+	}
+}

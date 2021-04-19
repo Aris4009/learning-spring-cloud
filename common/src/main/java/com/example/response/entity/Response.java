5071d98190565cb4df8357b65d34071f86e06f0a
he <slut@vip.qq.com>
2021-04-19 18:17:34 +0800

    去除数据源配置

--- common/src/main/java/com/example/response/entity/Response.java
+++ common/src/main/java/com/example/response/entity/Response.java
@@ -150,4 +150,27 @@ public final class Response<T> {
 		return (Response<T>) Response.builder().data(data).path(path).message(s).status(200)
 				.timestamp(LocalDateTime.now().toString()).build();
 	}
+
+	public static <T> Response<T> fail(T data, int status, String... msg) {
+		String s = null;
+		if (msg != null && msg.length > 0) {
+			StringBuilder builder = new StringBuilder();
+			for (String m : msg) {
+				builder.append(m);
+			}
+			s = builder.toString();
+		} else {
+			s = "fail";
+		}
+		return (Response<T>) Response.builder().data(data).message(s).status(status)
+				.timestamp(LocalDateTime.now().toString()).build();
+	}
+
+	public static Response<Void> fail(int status, String... msg) {
+		return fail(null, status, msg);
+	}
+
+	public static Response<Void> fail(String... msg) {
+		return fail(null, 500, msg);
+	}
 }
--- spring-cloud-gateway/src/main/java/com/example/App.java
+++ spring-cloud-gateway/src/main/java/com/example/App.java
@@ -5,10 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisReactiveAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, RedissonAutoConfiguration.class,
-		RedisAutoConfiguration.class, RedisReactiveAutoConfiguration.class })
+@SpringBootApplication(exclude = {RedissonAutoConfiguration.class, RedisAutoConfiguration.class,
+		RedisReactiveAutoConfiguration.class})
 
 public class App {
 	public static void main(String[] args) {

8cfeaeb61d884324147170371a10cd280cf51ca0
he <slut@vip.qq.com>
2021-05-10 15:46:30 +0800

    添加orm代码生成模板

---
+++ code-gen/src/main/java/com/example/CodeGen.java
@@ -0,0 +1,123 @@
+package com.example;
+
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.beetl.core.GroupTemplate;
+import org.beetl.core.ReThrowConsoleErrorHandler;
+import org.beetl.core.Template;
+import org.beetl.core.resource.FileResourceLoader;
+import org.beetl.sql.core.*;
+import org.beetl.sql.gen.BaseProject;
+import org.beetl.sql.gen.Entity;
+import org.beetl.sql.gen.SourceBuilder;
+import org.beetl.sql.gen.SourceConfig;
+import org.beetl.sql.gen.simple.BaseTemplateSourceBuilder;
+import org.beetl.sql.gen.simple.EntitySourceBuilder;
+import org.beetl.sql.gen.simple.MDSourceBuilder;
+import org.beetl.sql.gen.simple.SimpleMavenProject;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import cn.hutool.setting.dialect.Props;
+
+/**
+ * 自动生成orm
+ */
+public class CodeGen {
+
+	public static void main(String[] args) {
+		try {
+			HikariConfig hikariConfig = hikariConfig();
+			SQLManager sqlManager = sqlManager(hikariConfig);
+			initGroupTemplate();
+			genCode(sqlManager);
+			sqlManager.getDs().getMetaData().close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+	public static HikariConfig hikariConfig() {
+		Props props = new Props("db.properties");
+		return new HikariConfig(props);
+	}
+
+	public static SQLManager sqlManager(HikariConfig hikariConfig) {
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		ConnectionSource connectionSource = ConnectionSourceHelper.getSingle(dataSource);
+		SQLManagerBuilder sqlManagerBuilder = new SQLManagerBuilder(connectionSource);
+		sqlManagerBuilder.setNc(new UnderlinedNameConversion());
+		return sqlManagerBuilder.build();
+	}
+
+	public static void initGroupTemplate() {
+		GroupTemplate groupTemplate = BaseTemplateSourceBuilder.getGroupTemplate();
+		String root = System.getProperty("user.dir");
+		String templatePath = root + "/code-gen/src/main/resources/templates/";
+		FileResourceLoader resourceLoader = new FileResourceLoader(templatePath);
+		groupTemplate.setResourceLoader(resourceLoader);
+	}
+
+	public static void genCode(SQLManager sqlManager) {
+		List<SourceBuilder> sourceBuilder = new ArrayList<>();
+		SourceBuilder entityBuilder = new EntitySourceBuilder();
+		SourceBuilder mapperBuilder = new CustomMapperSourceBuilder("dao");
+		SourceBuilder mdBuilder = new MDSourceBuilder();
+
+		sourceBuilder.add(entityBuilder);
+		sourceBuilder.add(mapperBuilder);
+		sourceBuilder.add(mdBuilder);
+
+		SourceConfig config = new SourceConfig(sqlManager, sourceBuilder);
+		// 如果有错误，抛出异常而不是继续运行1
+		EntitySourceBuilder.getGroupTemplate().setErrorHandler(new ReThrowConsoleErrorHandler());
+
+		CustomProject project = new CustomProject("com.example");
+		project.setRoot(System.getProperty("user.dir") + "/services");
+		String tableName = "permission";
+		config.gen(tableName, project);
+	}
+
+	static class CustomProject extends SimpleMavenProject {
+		public CustomProject() {
+			super();
+		}
+		public CustomProject(String basePackage) {
+			super(basePackage);
+		}
+	}
+
+	static class CustomMapperSourceBuilder extends BaseTemplateSourceBuilder {
+
+		/**
+		 * 指定模板的路径
+		 */
+		public static String mapperPath = "mapper.btl";
+
+		public CustomMapperSourceBuilder(String name) {
+			super(name);
+		}
+
+		// Override
+		@Override
+		public void generate(BaseProject project, SourceConfig config, Entity entity) {
+
+			Template template = groupTemplate.getTemplate(mapperPath);
+			String mapperClass = entity.getName() + "Dao";
+			template.binding("className", mapperClass);
+			template.binding("package", project.getBasePackage(this.name));
+			template.binding("entityClass", entity.getName());
+			// 得到生成的entity的包
+			String entityPkg = project.getBasePackage("entity");
+			String mapperHead = entityPkg + ".*";
+			template.binding("imports", Arrays.asList(mapperHead));
+			Writer writer = project.getWriterByName(this.name, entity.getName() + "Dao.java");
+			template.renderTo(writer);
+
+		}
+	}
+}
---
+++ services/src/main/java/com/example/entity/Permission.java
@@ -0,0 +1,46 @@
+package com.example.entity;
+import org.beetl.sql.annotation.entity.AssignID;
+import org.beetl.sql.annotation.entity.Table;
+/*
+* 
+* gen by beetlsql3 2021-05-10
+*/
+
+@Table(name="permission")
+public class Permission implements java.io.Serializable {
+	@AssignID("snow")
+	private Long id ;
+
+	private String name ;
+
+	private String url ;
+
+	public Long getId(){
+		return  id;
+	}
+
+	public void setId(Long id ){
+		this.id = id;
+	}
+
+	public String getName(){
+		return  name;
+	}
+
+	public void setName(String name ){
+		this.name = name;
+	}
+
+	public String getUrl(){
+		return  url;
+	}
+
+	public void setUrl(String url ){
+		this.url = url;
+	}
+
+	@Override
+    public String toString() {
+        return JSON.toJSONString(this);
+    }
+}

ae0a01d5704b23a24acf38daac7e799de884c0eb
he <slut@vip.qq.com>
2021-05-12 18:10:57 +0800

    去除白名单机制，将白名单认证加入到网关中

--- spring-security/src/main/java/com/example/config/WebSecurityConfig.java
+++ spring-security/src/main/java/com/example/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.config;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,42 +10,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import com.example.entity.PermissionUrl;
-import com.example.json.JSON;
-import com.google.gson.reflect.TypeToken;
-
-import cn.hutool.core.io.resource.ClassPathResource;
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final Logger log = LoggerFactory.getLogger(WebSecurityConfig.class);
-
-	private final boolean enable;
-
-	private final List<PermissionUrl> permissionUrlList = new ArrayList<>();
-
-	private static final String ENABLE = "enable";
-
-	private static final String DISABLE = "disable";
-
-	public WebSecurityConfig(@Value("${spring.security.enable:false}") boolean enable,
-			@Value("${permission.url.file:permission-url.json}") String permissionUrlFile) {
-		this.enable = enable;
-		ClassPathResource classPathResource = new ClassPathResource(permissionUrlFile);
-		String str = classPathResource.readUtf8Str();
-		this.permissionUrlList.addAll(JSON.parse(str, new TypeToken<List<PermissionUrl>>() {
-		}.getType()));
-		log.info("init permission url success");
-	}
 
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource(
@@ -65,23 +39,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors(Customizer.withDefaults());
-		if (this.enable) {
-			super.configure(http);
-		} else {
-			http.authorizeRequests(request -> request.anyRequest().permitAll());
-		}
-		String flag;
-		if (this.enable) {
-			flag = ENABLE;
-		} else {
-			flag = DISABLE;
-		}
-		log.info("config spring security {}", flag);
-	}
-
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		String[] urls = this.permissionUrlList.stream().map(PermissionUrl::getUrl).toArray(String[]::new);
-		web.ignoring().antMatchers(urls);
+		http.authorizeRequests(request -> request.anyRequest().permitAll());
 	}
 }

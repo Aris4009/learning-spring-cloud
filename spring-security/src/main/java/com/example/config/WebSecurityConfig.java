4864add18d33dd3098b45a252b193ea5f2a96b94
he <slut@vip.qq.com>
2021-05-12 18:14:41 +0800

    去除白名单机制，将白名单认证加入到网关中

--- spring-security/src/main/java/com/example/config/WebSecurityConfig.java
+++ spring-security/src/main/java/com/example/config/WebSecurityConfig.java
@@ -8,8 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -37,8 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.cors(Customizer.withDefaults());
-		http.authorizeRequests(request -> request.anyRequest().permitAll());
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().anyRequest();
 	}
 }

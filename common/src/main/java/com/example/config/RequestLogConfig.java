ecd59be564e83d46fe450e21b1b4318a2c423e5a
he <slut@vip.qq.com>
2021-05-31 18:16:43 +0800

    开启nacos配置中心

--- common/src/main/java/com/example/config/RequestLogConfig.java
+++ common/src/main/java/com/example/config/RequestLogConfig.java
@@ -2,7 +2,6 @@ package com.example.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import lombok.Data;
 
@@ -10,7 +9,6 @@ import lombok.Data;
  * 请求日志配置
  */
 @Configuration
-@PropertySource(value = "classpath:request-log.properties", ignoreResourceNotFound = true)
 @Data
 public class RequestLogConfig {
 

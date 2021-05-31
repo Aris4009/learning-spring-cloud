2db9d63d8feb052dc31a8f3d111b74016d84714c
he <slut@vip.qq.com>
2021-05-31 14:06:08 +0800

    为其他子模块添加版本信息

--- common/src/main/java/com/example/git/GitInfoConfig.java
+++ common/src/main/java/com/example/git/GitInfoConfig.java
@@ -10,6 +10,15 @@ import lombok.Data;
 @PropertySource(value = "classpath:git.properties", ignoreResourceNotFound = true)
 @Data
 public class GitInfoConfig {
-	@Value("${git.commit.id:null}")
+	@Value("${git.commit.id.abbrev:null}")
 	private String commitId;
+
+	@Value("${git.commit.user.name:null}")
+	private String username;
+
+	@Value("${git.commit.time:null}")
+	private String time;
+
+	@Value("${git.commit.message.full:null}")
+	private String msg;
 }

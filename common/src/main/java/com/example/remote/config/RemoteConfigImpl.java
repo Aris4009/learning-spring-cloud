e19edd31cbd7958c62942799ff4a385b5bb8b629
he <slut@vip.qq.com>
2021-06-25 10:50:17 +0800

    完成nacos自定义远程配置读取

--- common/src/main/java/com/example/remote/config/RemoteConfigImpl.java
+++ common/src/main/java/com/example/remote/config/RemoteConfigImpl.java
@@ -1,12 +1,15 @@
 package com.example.remote.config;
 
 import java.io.File;
-import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,19 +23,15 @@ import com.google.gson.reflect.TypeToken;
 
 import cn.hutool.core.io.IORuntimeException;
 import cn.hutool.core.io.resource.ClassPathResource;
-import cn.hutool.http.HttpUtil;
 
 public class RemoteConfigImpl implements IRemoteConfig {
 
-	private final String type;
-
-	private final String path;
+	private RemoteConfigPro remoteConfigPro;
 
 	private final Map<String, Object> cache = new ConcurrentHashMap<>();
 
-	public RemoteConfigImpl(String type, String path) {
-		this.type = type;
-		this.path = path;
+	public RemoteConfigImpl(RemoteConfigPro remoteConfigPro) {
+		this.remoteConfigPro = remoteConfigPro;
 	}
 
 	private static final Logger log = LoggerFactory.getLogger(RemoteConfigImpl.class);
@@ -43,17 +42,22 @@ public class RemoteConfigImpl implements IRemoteConfig {
 
 	private static final String SERVER_ADDR = "serverAddr";
 
-	private static final String DATA_ID = "dataId";
+	private static final String NAMESPACE = "namespace";
 
-	private static final String GROUP = "group";
-
-	private static final long timeout = 5000L;
+	private static final long TIMEOUT = 5000L;
 
 	@Override
+	@PostConstruct
 	public void init() throws BusinessException {
-		if (type == null || path == null) {
+		if (remoteConfigPro == null) {
 			return;
 		}
+		String type = remoteConfigPro.getType();
+		String path = remoteConfigPro.getPath();
+		String serverAddr = remoteConfigPro.getServerAddr();
+		String namespace = remoteConfigPro.getNamespace();
+		String dataId = remoteConfigPro.getDataId();
+		String group = remoteConfigPro.getGroup();
 		if (type.equalsIgnoreCase(FILE_TYPE)) {
 			try {
 				ClassPathResource classPathResource = new ClassPathResource(path);
@@ -72,30 +76,34 @@ public class RemoteConfigImpl implements IRemoteConfig {
 			}
 		} else if (type.equalsIgnoreCase(SERVER_TYPE)) {
 			try {
-				Map<String, String> params = HttpUtil.decodeParamMap(path, StandardCharsets.UTF_8);
-				String serverAddr = params.get(SERVER_ADDR);
 				Properties properties = new Properties();
 				properties.setProperty(SERVER_ADDR, serverAddr);
-				String dataId = params.get(DATA_ID);
-				String group = params.get(GROUP);
+				properties.setProperty(NAMESPACE, namespace);
 				ConfigService configService = NacosFactory.createConfigService(properties);
-				String content = configService.getConfig(dataId, group, timeout);
+				String content = configService.getConfig(dataId, group, TIMEOUT);
 				this.cache.clear();
 				if (content != null) {
 					this.cache.putAll(JSON.parse(content, new TypeToken<Map<String, String>>() {
 					}));
 				}
-				configService.addListener(dataId, group, new ListenerImpl(this.cache));
+				configService.addListener(dataId, group, new ListenerImpl(type, path, this.cache));
 			} catch (Exception e) {
 				throw new BusinessException(e);
 			}
 		} else {
 			throw new BusinessException("unsupported remote config type:" + type + ",it must be file or server");
 		}
+		log.info("init remote config cache type:{}，path:{},cache size:{}", type, path, this.cache.size());
 	}
 
 	@Override
+	@PreDestroy
 	public void destroy() {
+		if (remoteConfigPro == null) {
+			return;
+		}
+		log.info("clear remote config cache type:{},path:{}", Optional.of(remoteConfigPro.getType()).orElse(null),
+				Optional.of(remoteConfigPro.getPath()).orElse(null));
 		this.cache.clear();
 	}
 
@@ -106,9 +114,15 @@ public class RemoteConfigImpl implements IRemoteConfig {
 
 	static class ListenerImpl implements Listener {
 
+		private final String type;
+
+		private final String path;
+
 		private final Map<String, Object> cache;
 
-		public ListenerImpl(Map<String, Object> cache) {
+		public ListenerImpl(String type, String path, Map<String, Object> cache) {
+			this.type = type;
+			this.path = path;
 			this.cache = cache;
 		}
 
@@ -119,6 +133,7 @@ public class RemoteConfigImpl implements IRemoteConfig {
 
 		@Override
 		public void receiveConfigInfo(String configInfo) {
+			log.info("refresh remote config cache type:{},path:{}", this.type, this.path);
 			this.cache.clear();
 			if (configInfo != null) {
 				this.cache.putAll(JSON.parse(configInfo, new TypeToken<Map<String, String>>() {
--- common/src/main/java/com/example/remote/config/RemoteConfiguration.java
+++ common/src/main/java/com/example/remote/config/RemoteConfiguration.java
@@ -7,9 +7,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RemoteConfiguration {
 
-	@Bean(value = "remoteUrlConfig", destroyMethod = "destroy")
+	@Bean(value = "remoteUrlConfig")
 	public IRemoteConfig remoteUrlConfig(@Value("${remote-url.internal.type:#{null}}") String type,
-			@Value("${remote-url.internal.path:#{null}}") String path) {
-		return new RemoteConfigImpl(type, path);
+			@Value("${remote-url.internal.path:#{null}}") String path,
+			@Value("${remote-url.internal.serverAddr:#{null}}") String serverAddr,
+			@Value("${remote-url.internal.namespace:#{null}}") String namespace,
+			@Value("${remote-url.internal.dataId:#{null}}") String dataId,
+			@Value("${remote-url.internal.group:#{null}}") String group) {
+		RemoteConfigPro remoteConfigPro = RemoteConfigPro.builder().type(type).path(path).serverAddr(serverAddr)
+				.namespace(namespace).dataId(dataId).group(group).build();
+		return new RemoteConfigImpl(remoteConfigPro);
 	}
 }
